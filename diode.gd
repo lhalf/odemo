b86481508e85f8f4183f741edc285bdbbb1708bd
Laurence Halfpenny <laurence.halfpenny@halfpenny.net>
2024-10-03 21:57:45 +0100

    refactor

--- diode.gd
+++ diode.gd
@@ -1,18 +1,20 @@
 extends MeshInstance3D
 
-@onready var input_light = %InputLight
+@onready var input_light: OmniLight3D = %InputLight
+@onready var output_light: OmniLight3D = %OutputLight
 @onready var flash_timer = %FlashTimer
 @onready var animation_player = %AnimationPlayer
 @onready var output_wire = %Wire
 
-@export var flashing: bool :
+@export var input_flashing: bool :
 	set(value):
-		flashing = value
-		if flashing:
-			flash_timer.start()
-		else:
-			input_light.visible = false
-			flash_timer.stop()
+		input_flashing = value
+		input_light.visible = input_flashing
+
+@export var output_flashing: bool :
+	set(value):
+		output_flashing = value
+		output_light.visible = output_flashing
 
 @export var cables_connected: bool :
 	set(value):
@@ -26,10 +28,16 @@ extends MeshInstance3D
 			animation_player.play_backwards("insert_cables")
 
 func _set_output_active(to: bool) -> void:
-	if to and output_wire.get_active_material(0).get_next_pass().get_shader_parameter("shine_speed") == 0:
+	if to and not _is_output_active():
+		output_flashing = true
 		output_wire.get_active_material(0).get_next_pass().set_shader_parameter("shine_speed", -3)
-	if not to and output_wire.get_active_material(0).get_next_pass().get_shader_parameter("shine_speed") == -3:
+	if not to and _is_output_active():
+		output_flashing = false
 		output_wire.get_active_material(0).get_next_pass().set_shader_parameter("shine_speed", 0)
 
 func _on_flash_timer_timeout():
-	input_light.visible = !input_light.visible
+	input_light.light_energy = 1 if input_light.light_energy == 0 else 0
+	output_light.light_energy = 1 if output_light.light_energy == 0 else 0
+
+func _is_output_active() -> bool:
+	return output_wire.get_active_material(0).get_next_pass().get_shader_parameter("shine_speed") == -3
